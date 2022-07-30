d48d77fcc51772f2f4c22c37a0a0bb093f1ed739
Tom Hughes <tomhughes1987@gmail.com>
2022-07-30 13:36:45 +0200

    add section 8 notes and practices

---
+++ sec8-enums.ts
@@ -0,0 +1,34 @@
+
+//not commonly used
+
+enum playerStatus{
+    WIN, //0
+    LOSE, //1
+    DRAW, //2
+    RAGE_QUIT, //3
+}
+
+const Dan = playerStatus.LOSE;
+const Tom = playerStatus.WIN;
+
+/////////////////
+
+enum direction{
+    LEFT='left', //left
+    RIGHT='right',
+    U_TURN='turn around 180 degrees',
+    FORWARD='straight on',
+}
+//enums creates lots of extra code in js, some dont like that and .ts is moving away from that
+//the 'direction' enum above turns into this:
+
+// "use strict";
+// var direction;
+// (function (direction) {
+//     direction["LEFT"] = "left";
+//     direction["RIGHT"] = "right";
+//     direction["U_TURN"] = "turn around 180 degrees";
+//     direction["FORWARD"] = "straight on";
+// })(direction || (direction = {}));
+
+// using 'const' in front of enum keep .js much shorter (on the output), more research needed
---
+++ sec8-tuples.ts
@@ -0,0 +1,25 @@
+//not in js!
+// fix what type is in an array:
+
+let colour:['rgb:',number,number,number];
+
+colour=['rgb:',0,254,255]
+colour=['rgb:',0,0,255]
+colour=['rgb:',255,255,255]
+
+////////////////////////
+
+let hex: ['#'|''| undefined, number|string,number|string,number|string,]
+
+hex = ['#',10,'fe','a3']
+hex = ['','ff','fe',0o0]
+hex = [,10,'fe','a3']
+
+// the 0o0 thing? banana 
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Deprecated_octal
+
+///////////////
+// ODD THING!
+///////////////
+//you can not change a tuple... BUT you can use array.pop() and array.push()
+
