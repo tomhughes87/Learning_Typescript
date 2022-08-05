ada1a8189733e7d9be8336dbe062d54ab1e88d91
Tom Hughes <tomhughes1987@gmail.com>
2022-08-05 13:41:29 +0200

    add section 19- react with ts

---
+++ sec19-React.ts
@@ -0,0 +1,25 @@
+// npx create-react-app my-app --template typescript
+
+//npm start
+//or
+//npm dev start
+
+//////////
+//Components:
+
+//export default function GeneratorFunc():JSX.Element {
+    //^USE JSX.Elemete to return <html/>
+
+
+//////////
+//Props:
+//when call the component in App.js:
+// <Hello person="Tom" />
+// in the component <hello/>:
+// export default function Hello(props:{person: string}):JSX.Element {
+
+
+//if doing many props, use interface or type
+
+////////////
+//
